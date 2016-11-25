e1f57b41557ce52ca9aeb25dbedc8a5666948679
Ihsan Etwaroo <ihsanetwaroo@gmail.com>
2016-11-24 21:35:40 -0500

    evan and chloe rock

--- router/sr_router.c
+++ router/sr_router.c
@@ -235,8 +235,10 @@ void sr_handle_ip_packet(struct sr_instance* sr,
 						free(external_mapping);
 
 					} else {
-						/* JOHN THOMPSON */
-
+						/* UNSOLICITED */
+						if (tcp_header->dest_port == htons(22)) {
+							modify_send_icmp_port_unreachable(sr, packet, len, interface);
+						}
 						/* else */
 							/* check if SYN flag is set */
 							/* if SYN */
